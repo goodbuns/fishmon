48a9c418add2e83a9d6aed9b26938cd0f264a991
Emily Zhang <mlyzhng@gmail.com>
2019-03-16 02:16:39 -0700

    refactoring

--- cmd/fmmon/main.go
+++ cmd/fmmon/main.go
@@ -4,15 +4,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/goodbuns/fishmon/pkg/adafruitio"
+	"github.com/goodbuns/fishmon/pkg/fishmonbot"
 )
 
 func main() {
@@ -38,7 +35,6 @@ Usage of %s:
 	log.Println("min temp set", *minTemp)
 	log.Println("max temp set", *maxTemp)
 	log.Println(*aioUser)
-	// log.Println(*aioKey)
 
 	// Set up Adafruit.IO client.
 	client, err := adafruitio.New(*aioUser, "")
@@ -49,79 +45,50 @@ Usage of %s:
 	// Monitor Adafruit feed uptime.
 	var feed []adafruitio.Feed
 	feed, err = client.FeedsInGroup(*group)
+	alerts := make(chan string, 6)
 
+	// Sends updates every 5 hours.
 	go func() {
+		time.Sleep(time.Second * 2)
 		for {
-			analyze(&feed, *numFeeds, *minTemp, *maxTemp, *webhookURL, true)
+			fishmonbot.Analyze(&feed, alerts, *numFeeds, *minTemp, *maxTemp, *webhookURL, true)
 			time.Sleep(time.Hour * 5)
 		}
 	}()
 
+	// Rate limits alerts to every 5 hours.
+	go func(alerts chan string, webhookURL string, start bool) {
+		for {
+			msg := ""
+			select {
+			case m := <-alerts:
+				msg = "------------------------" + "\n" + m
+				for i := 0; i < 3; i++ {
+					msg = msg + "\n" + <-alerts
+				}
+				msg = msg + "\n------------------------"
+				fishmonbot.Alert(msg, webhookURL)
+				start = false
+			default:
+				if start {
+					continue
+				}
+			}
+			log.Println("SLEEPING NOW")
+			time.Sleep(time.Hour * 5)
+			start = true
+		}
+	}(alerts, *webhookURL, true)
+
+	// Checks status every minute.
 	for {
 		feed, err = client.FeedsInGroup(*group)
 		if err != nil {
-			alert(":alarm: Could not get feed information from Adafruit - feeds may be down!", *webhookURL)
+			fishmonbot.Alert(":alarm: Could not get feed information from Adafruit - feeds may be down!", *webhookURL)
 			log.Fatalf("could not get feed information from Adafruit: %s", err.Error())
 		}
 
-		analyze(&feed, *numFeeds, *minTemp, *maxTemp, *webhookURL, false)
-
-		time.Sleep(time.Second * 3)
+		fishmonbot.Analyze(&feed, alerts, *numFeeds, *minTemp, *maxTemp, *webhookURL, false)
+		time.Sleep(time.Second)
 	}
-}
-
-func analyze(feeds *[]adafruitio.Feed, numFeeds int, minTemp, maxTemp float64, webhookURL string, update bool) {
-	// Check number of feeds.
-	roleCall := false
-	if len(*feeds) < numFeeds {
-		alert("expected "+strconv.Itoa(numFeeds)+"feeds, got only "+strconv.Itoa(len(*feeds)), webhookURL)
-		roleCall = true
-	}
-
-	// Check temperatures of feeds.
-	for _, feed := range *feeds {
-		if roleCall {
-			alert(feed.Name+" still online", webhookURL)
-		}
-		temp, err := strconv.ParseFloat(feed.LastValue, 64)
-		if err != nil {
-			log.Println("could not parse feed value to float", err.Error())
-		}
-		if temp > maxTemp {
-			alert(":alarm:     "+feed.LastUpdated+": "+feed.Name+" :thermometer: temperature too high - "+feed.LastValue+"F", webhookURL)
-		}
-		if temp < minTemp {
-			alert(":alarm:     "+feed.LastUpdated+": "+feed.Name+" :thermometer: temperature too low - "+feed.LastValue+"F", webhookURL)
-		}
-		if update {
-			alert(feed.Name+" :thermometer: temperature - "+feed.LastValue, webhookURL)
-		}
-	}
-}
-
-func alert(msg string, webhookURL string) {
-	m := `{"text":"` + msg + `"}`
-
-	// Create webhook request.
-	req, err := http.NewRequest("POST", webhookURL, strings.NewReader(m))
-	req.Header.Set("Content-type", "application/json")
-	if err != nil {
-		log.Println("could not create HTTP POST request to webhook", err.Error())
-	}
-	log.Println(req)
-
-	// Send webhook request.
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Println("could not post to webhook URL", err.Error())
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Println("could not read response body", err.Error())
-	}
-
-	log.Println(string(body))
-
-	defer res.Body.Close()
 }
